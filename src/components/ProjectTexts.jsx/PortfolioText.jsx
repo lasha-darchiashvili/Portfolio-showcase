61271ef73abacd4687a591a2c81ab943db5ec548
lasha-darchiashvili <ldarc17@freeuni.edu.ge>
2023-02-16 01:05:55 +0400

    FIX: minor fix

--- src/components/ProjectTexts.jsx/PortfolioText.jsx
+++ src/components/ProjectTexts.jsx/PortfolioText.jsx
@@ -3,10 +3,9 @@ import React from "react";
 const PortfolioText = () => {
   return (
     <p className="w-[85%] ">
-      A visually stunning portfolio website, crafted using the{" "}
+      A fully responsive portfolio website, crafted using the{" "}
       <span className="text-green">React</span> and styled with the{" "}
-      <span className="text-green">Tailwind CSS. </span>The website also
-      features elegant animations.
+      <span className="text-green">Tailwind CSS. </span>
     </p>
   );
 };
